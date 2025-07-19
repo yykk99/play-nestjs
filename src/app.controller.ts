ea3edd83fe50f0be2b473949c4dd151f435f4289
yykk99 <yuuuki96g@gmail.com>
2025-07-19 09:59:39 +0900

    userモジュール作成

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  findAll(): string {
     return this.appService.getHello();
   }
 }
